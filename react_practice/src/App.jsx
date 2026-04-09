8f3491a67065b267cb3ebb36ca3f4c04fec70e2b
krish <krishpatel3300@gmail.com>
2026-04-09 20:45:04 +0530

    made changes in the css.0

--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -23,26 +23,26 @@ function App() {
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-8 my-8 text-orange-500 bg-gray-700'>
-      </div>
-      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-        <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='password' readOnly />
-        <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
-      </div>
-      <div className='flex text-sm gap-x-2'>
-        <div className='flex items-center gap-x-1'>
-          <input type="range"
-            min={6}
-            max={100}
-            value={length}
-            className='cursor-pointer'
-            onChange={(e) => { setLength(e.target.value) }}
-          />
-          <label>Length: {length}</label>
+        <h1 className='text-white text-center my-3 text-xl'>Password Generator</h1>
+        
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input type="text" value={password} className='outline-none w-full py-1 px-3 bg-white text-gray-800' placeholder='password' readOnly />
+          <button className='outline-none bg-blue-700 hover:bg-blue-800 text-white px-3 py-0.5 shrink-0 transition-colors'>copy</button>
         </div>
-
+        
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input type="range"
+              min={6}
+              max={100}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }}
+            />
+            <label>Length: {length}</label>
+          </div>
+        </div>
       </div>
-
-
     </>
   )
 }
