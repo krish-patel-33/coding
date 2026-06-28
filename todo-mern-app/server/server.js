5b820855ddc89a66069c1d7b6dcc0696a6876eba
krish <krishpatel3300@gmail.com>
2026-06-28 15:52:57 +0530

    cretaed thr todo file structure and server and first route

---
+++ todo-mern-app/server/server.js
@@ -0,0 +1,13 @@
+const express = require ("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.send("API is running ");
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
